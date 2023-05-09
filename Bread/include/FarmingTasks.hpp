54df47098779fbb745018f00f72bd61bec825bdc
Gaspard Culis <79080828+GaspardCulis@users.noreply.github.com>
2023-05-09 09:16:21 +0000

    Added FarmingTasks

---
+++ Bread/include/FarmingTasks.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include <botcraft/AI/BehaviourTree.hpp>
+#include "AdvancedClient.hpp"
+
+/// @brief Check condition
+/// @param client The client performing the action
+/// @param radius The max distance to search blocks
+/// @return Returns success if all blocks have been found
+Botcraft::Status InitializeBlocks(AdvancedClient& client, const int radius = 128);
+
+/// @brief Go fishing
+/// @param client The client performing the action
+/// @return Returns success if fish has been aquired
+Botcraft::Status Fish(AdvancedClient& client, const std::string& block_pos_blackboard);
+
+/// @brief Break carrots/potatoes/wheat/pumpkin/melon, collect the items, replant
+/// @param client The client performing the action
+/// @param blocks_pos_blackboard Blackboard address of the workstation block position
+/// @param item_name Name of the item we want to collect
+/// @return Success if blocks are broken, items gathered and crops replanted, Failure otherwise
+Botcraft::Status CollectCropsAndReplant(Botcraft::BehaviourClient& client, const std::string& block_pos_blackboard, const int crops_radius = 8);
---
+++ Bread/src/FarmingTasks.cpp
@@ -0,0 +1,24 @@
+#include "FarmingTasks.hpp"
+
+Botcraft::Status InitializeBlocks(AdvancedClient& client, const int search_radius) {
+    Botcraft::Blackboard& b = client.GetBlackboard();
+
+    Position fishing_workstation_position;
+    Position farming_workstation_position;
+
+    try {
+        client.findBlocks([&client, &fishing_workstation_position, &farming_workstation_position](const Block *block, const Position position) -> bool {
+            if (block->GetBlockstate()->GetName() == "minecraft:barrel" && client.getBlock(position + Position(0, -1, 0))->GetBlockstate()->GetName() == "minecraft:gold_block") {
+                fishing_workstation_position = position;
+                LOG_INFO("Fishing workstation found at: " << fishing_workstation_position << "!");
+            } else if (block->GetBlockstate()->GetName() == "minecraft:composter" && client.getBlock(position + Position(0, -1, 0))->GetBlockstate()->GetName() == "minecraft:gold_block") {
+                farming_workstation_position = position;
+                LOG_INFO("Farming workstation found at: " << farming_workstation_position << "!");
+            }
+
+            return false;
+        }, search_radius);
+    } catch(std::exception &e) {
+        LOG_ERROR("Error while searching workstations: " << e.what());
+    }
+}
