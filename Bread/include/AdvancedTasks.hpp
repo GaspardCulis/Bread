6d3870cbfbc1257508bb5221432b25fbfa4ff158
Gaspard Culis <79080828+GaspardCulis@users.noreply.github.com>
2023-05-11 12:34:23 +0000

    Added AdvancedTasks::CollectItem procedure and improved AdvancedTasks::DigAndCollect

--- Bread/include/AdvancedTasks.hpp
+++ Bread/include/AdvancedTasks.hpp
@@ -2,13 +2,18 @@
 #include <botcraft/AI/BehaviourTree.hpp>
 #include "AdvancedClient.hpp"
 
-namespace AdvancedTasks {
+namespace AdvancedTasks
+{
 
-/// @brief Digs a given block and tries to collect the drop. Pathfinds to the block and the item drop.
-/// @param client The client performing the action
-/// @param position The block position
-/// @param item_name The drop item name
-/// @return Success if the bed found, false otherwise
-Botcraft::Status DigAndCollect(AdvancedClient& client, const Position position, const std::string &item_name);
-    
+    /// @brief Pathfinds to an item and wait for pickup
+    /// @param id The item's entity id
+    /// @returns Success if item has been collected, Failure otherwise (4s collect timeout)
+    Botcraft::Status CollectItem(AdvancedClient &client, int id);
+
+    /// @brief Digs a given block and tries to collect the drop. Pathfinds to the block and the item drop. Automatically finds new item drops
+    /// @param client The client performing the action
+    /// @param position The block position
+    /// @return Success if the bed found, false otherwise
+    Botcraft::Status DigAndCollect(AdvancedClient &client, const Position position);
+
 }
--- Bread/src/AdvancedTasks.cpp
+++ Bread/src/AdvancedTasks.cpp
@@ -3,62 +3,82 @@
 #include "botcraft/Game/AssetsManager.hpp"
 #include "botcraft/Game/Entities/EntityManager.hpp"
 
-Botcraft::Status AdvancedTasks::DigAndCollect(AdvancedClient& client, const Position position, const std::string &item_name) {
-    if (Botcraft::Dig(client, position) == Status::Failure) {
-        LOG_WARNING("Error trying to break block at " << position);
-        return Status::Failure;
-    }
+Botcraft::Status AdvancedTasks::CollectItem(AdvancedClient &client, int id)
+{
+    std::shared_ptr<EntityManager> entity_manager = client.GetEntityManager();
 
-    for (int i = 0; i < 50; ++i) {
-        client.Yield();
-    }
+    Vector3<double> entity_position = client.getEntity(id)->GetPosition();
 
-    Position entity_position;
-    int entity_id;
-    std::set<int> entities = client.findEntities(
-        [item_name, &entity_position, &entity_id](const std::shared_ptr<Entity> entity) -> bool {
-            if (entity->GetType() == EntityType::ItemEntity) {
-                std::shared_ptr<ItemEntity> item = std::static_pointer_cast<ItemEntity>(entity);
-#if PROTOCOL_VERSION < 340
-                if (AssetsManager::getInstance().Items().at(item->GetDataItem().GetBlockID()).at(item->GetDataItem().GetItemDamage())->GetName() == item_name)
-#else
-                if (AssetsManager::getInstance().Items().at(item->GetDataItem().GetItemID())->GetName() == item_name)
-#endif
-                {
-                    entity_position = entity->GetPosition();
-                    entity_id = entity->GetEntityID();
-                    return true;
-                }
-            }
-            return false;
-        }, 1);
-
-    if (entities.size() != 1) {
-        LOG_WARNING("Couldn't find drop for item "<<item_name<<" while digging block at "<<position<<"!");
-        return Status::Failure;
-    }
-
-    //Pathfind to the drop
-
-    if (GoTo(client, entity_position, 2) == Status::Failure) {
+    // Pathfind to the drop
+    if (GoTo(client, entity_position, 2) == Status::Failure)
+    {
         LOG_WARNING("Error trying to pick up block drop (can't get close enough to " << entity_position << ")");
         return Status::Failure;
     }
-    
-    // Wait for item pickup
 
+    // Wait for item pickup
     auto start = std::chrono::steady_clock::now();
-    while (true) {
-        if (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - start).count() >= 5000) {
+    while (true)
+    {
+        if (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - start).count() >= 4000)
+        {
             LOG_WARNING("Error waiting for block drop pick-up (Timeout).");
             return Status::Failure;
         }
 
-        if(client.getEntity(entity_id) == nullptr) {
+        if (client.getEntity(id) == nullptr)
+        {
             break;
         }
 
         client.Yield();
+    }
+
+    return Status::Success;
+}
+
+Botcraft::Status AdvancedTasks::DigAndCollect(AdvancedClient &client, const Position position)
+{
+    if (Botcraft::Dig(client, position, true) == Status::Failure)
+    {
+        LOG_WARNING("Error trying to break block at " << position);
+        return Status::Failure;
+    }
+
+    for (int i = 0; i < 50; ++i)
+    {
+        client.Yield();
+    }
+
+    std::set<int> entities = client.findEntities(
+        [&position](const std::shared_ptr<Entity> entity) -> bool
+        {
+            if (entity->GetType() == EntityType::ItemEntity)
+            {
+                std::shared_ptr<ItemEntity> item = std::static_pointer_cast<ItemEntity>(entity);
+                if (position.SqrDist(item->GetPosition()) <= 2)
+                {
+                    return true;
+                }
+            }
+            return false;
+        },
+        1);
+
+    if (entities.size() != 1)
+    {
+        LOG_WARNING("Couldn't find drop after breaking block at " << position << "!");
+        return Status::Failure;
+    }
+
+    // Collect the drops
+    for (int eid : entities)
+    {
+        if (AdvancedTasks::CollectItem(client, eid) == Status::Failure)
+        {
+            LOG_WARNING("Failed to collect item drop after digging block at " << position);
+            return Status::Failure;
+        }
     }
 
     return Status::Success;
