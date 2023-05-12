fe94b31bd74ed18128e9669dc420204c51a3e964
Gaspard Culis <79080828+GaspardCulis@users.noreply.github.com>
2023-05-12 09:33:04 +0000

    Smol docs improvements

--- Bread/include/AdvancedClient.hpp
+++ Bread/include/AdvancedClient.hpp
@@ -22,26 +22,26 @@ public:
     AdvancedClient(const bool use_renderer_ = false);
 
     /**
-     * Returns the client current position in the world
+     * @brief Returns the client current position in the world
      */
     Vector3<double> getPosition() const;
 
     /**
-     * Gets the block at a given position
+     * @brief Gets the block at a given position
      */
     const Block *getBlock(Vector3<int> position) const;
 
     /**
-     * Gets the entity with the given id
+     * @brief Gets the entity with the given id
      */
     const shared_ptr<Entity> getEntity(int id) const;
 
     /**
      * @brief Finds all the blocks for which the match_function callback returned true in a certain radius.
-     * @param match_function If this function given the current Block returns true, the Block will be added to the result. Callback parameters:
-     * @param block: Pointer to the current Block being evaluated.
-     * @param position: The position of the current Block.
-     * @param world: Shared pointer to the World object being searched. Use it as the world Mutex is otherwise locked.
+     * @param match_function If this function given the current Block returns true, the Block will be added to the result. Callback parameters are explained in the Model Parameters
+     * @tparam block: Pointer to the current Block being evaluated.
+     * @tparam position: The position of the current Block.
+     * @tparam world: Shared pointer to the World object being searched. Use it as the world Mutex is otherwise locked.
      * @param search_radius The maximum distance for the search, it's a cube not a sphere.
      * @param max_results The maximum results for the search, the function returns when reached.
      * @param origin The search radius center, defaults to the player position
@@ -49,9 +49,12 @@ public:
     vector<Vector3<int>> findBlocks(std::function<bool(const Block *block, const Position position, std::shared_ptr<World> world)> match_function, const int search_radius = 64, const int max_results = -1, const std::optional<Position> origin = std::nullopt) const;
 
     /**
-     * Finds all the blocks matching the block_name (example: "minecraft:white_stained_glass", the minecraft: is required), in a certain radius.
+     * Finds all the blocks matching the block_name in a certain radius.
      * The search will stop and return when the number of found blocks equals max_results.
-     *
+     * @param block_name The searched block name (Ex: "minecraft:pumpkin")
+     * @param search_radius The maximum distance for the search, it's a cube not a sphere.
+     * @param max_results The maximum results for the search, the function returns when reached.
+     * @param origin The search radius center, defaults to the player position
      * Alias for findBlocks(std::function<bool(const Block *block, const Position position)>, int, int)
      */
     vector<Vector3<int>> findBlocks(const string block_name, const int search_radius = 64, const int max_results = -1, const std::optional<Position> origin = std::nullopt) const;
