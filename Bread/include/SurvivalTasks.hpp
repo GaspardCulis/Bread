eeb844e8823a2704b6da24ae187fa77d27f5d7ff
Gaspard Culis <79080828+GaspardCulis@users.noreply.github.com>
2023-05-12 12:39:54 +0000

    Big FarmingTasks tree improvements

--- Bread/include/SurvivalTasks.hpp
+++ Bread/include/SurvivalTasks.hpp
@@ -10,6 +10,11 @@ namespace SurvivalTasks
     /// @return Always success
     Botcraft::Status InitializeBlocks(AdvancedClient &client, const int search_radius = 128);
 
+    /// @brief Searches the client's inventory for the most nutritious food, saves the item name in Blackboard value "SurvivalTasks.best_food_in_inventory"
+    /// @param client The client performing the action
+    /// @return Failure if no edible food in the inventory
+    Botcraft::Status FindBestFoodInInventory(AdvancedClient &client);
+
     std::shared_ptr<Botcraft::BehaviourTree<AdvancedClient>> CreateTree();
 
 }
--- Bread/src/SurvivalTasks.cpp
+++ Bread/src/SurvivalTasks.cpp
@@ -1,5 +1,44 @@
 #include "SurvivalTasks.hpp"
 
+// https://minecraft.fandom.com/wiki/Food
+const std::vector<std::string> edible_items({"minecraft::rabbit_stew",
+                                             "minecraft::cooked_porkchop",
+                                             "minecraft::cooked_beef",
+                                             "minecraft::golden_carrot",
+                                             "minecraft::cooked_mutton",
+                                             "minecraft::cooked_salmon",
+                                             // "minecraft::echanted_golden_apple", // Not gonna eat that right?
+                                             "minecraft::beetroot_soup",
+                                             "minecraft::cooked_chicken",
+                                             "minecraft::mushroom_stew",
+                                             "minecraft::suspicious_stew",
+                                             "minecraft::pumpkin_pie",
+                                             "minecraft::baked_potato",
+                                             "minecraft::cooked_cod",
+                                             "minecraft::cooked_rabbit",
+                                             "minecraft::honey_bottle",
+                                             "minecraft::carrot",
+                                             "minecraft::apple",
+                                             "minecraft::chorus_fruit",
+                                             "minecraft::raw_beef",
+                                             "minecraft::raw_porkchop",
+                                             "minecraft::raw_rabbit",
+                                             "minecraft::rotten_flesh",
+                                             "minecraft::melon_slice",
+                                             "minecraft::poisonous_potato",
+                                             "minecraft::raw_chicken",
+                                             "minecraft::raw_mutton",
+                                             "minecraft::cookie",
+                                             "minecraft::glow_berries",
+                                             "minecraft::raw_cod",
+                                             "minecraft::raw_salmon",
+                                             "minecraft::sweet_berries",
+                                             "minecraft::beetroot",
+                                             "minecraft::dried_kelp",
+                                             "minecraft::potato",
+                                             "minecraft::pufferfish",
+                                             "minecraft::tropical_fish"});
+
 Botcraft::Status SurvivalTasks::InitializeBlocks(AdvancedClient &client, const int search_radius)
 {
     Botcraft::Blackboard &b = client.GetBlackboard();
@@ -30,6 +69,72 @@ Botcraft::Status SurvivalTasks::InitializeBlocks(AdvancedClient &client, const i
     return Status::Success;
 }
 
+Botcraft::Status SurvivalTasks::FindBestFoodInInventory(AdvancedClient &client)
+{
+    int i = 0;
+    while (i < edible_items.size() && client.getItemSlotInInventory(edible_items[i]) < 0)
+    {
+        i++;
+    }
+    if (i == edible_items.size())
+    {
+        LOG_WARNING("[FindBestFoodInInventory] No edible food found in inventory");
+        return Status::Failure;
+    }
+
+    Botcraft::Blackboard &b = client.GetBlackboard();
+    b.Set<std::string>("SurvivalTasks.best_food_in_inventory", edible_items[i]);
+
+    return Status::Success;
+}
+
+// Absolutely not stolen code...
+std::shared_ptr<Botcraft::BehaviourTree<AdvancedClient>> CreateSleepTree()
+{
+    // clang-format off
+    return Botcraft::Builder<AdvancedClient>("sleep")
+        .selector()
+            // If it's night
+            .inverter().leaf("check if night", Botcraft::IsNightTime)
+            .sequence()
+                // Go to the bed
+                .leaf(Botcraft::CopyBlackboardData, "SurvivalTasks.bed_pos", "GoTo.goal") // See? Not the same
+                .leaf(Botcraft::SetBlackboardData<int>, "GoTo.dist_tolerance", 2)
+                .leaf("go to bed", Botcraft::GoToBlackboard)
+                // Right click the bed every second until it's day time
+                .repeater(0).sequence()
+                    .leaf(Botcraft::CopyBlackboardData, "DispenserFarmBot.bed_position", "InteractWithBlock.pos")
+                    .leaf(Botcraft::SetBlackboardData<bool>, "InteractWithBlock.animation", true)
+                    .leaf("interact with bed", Botcraft::InteractWithBlockBlackboard)
+                    // Wait ~1s
+                    .repeater(100).leaf(Botcraft::Yield)
+                    .inverter().leaf("check if night", Botcraft::IsNightTime)
+                .end()
+            .end()
+        .end();
+    // clang-format on
+}
+
+// Same
+std::shared_ptr<Botcraft::BehaviourTree<AdvancedClient>> CreateEatTree()
+{
+    // clang-format off
+    return Botcraft::Builder<AdvancedClient>("eat")
+        .selector()
+            // If hungry
+            .inverter().leaf("check is hungry", Botcraft::IsHungry)
+            // Go buy some food, then eat
+            .sequence()
+                .leaf("check if has 3 emeralds in inventory", Botcraft::HasItemInInventory, "minecraft:emerald", 3)
+                .leaf(SurvivalTasks::FindBestFoodInInventory)
+                .leaf(Botcraft::CopyBlackboardData, "SurvivalTasks.best_food_in_inventory", "Eat.food_name")
+                .leaf(Botcraft::SetBlackboardData<bool>, "Eat.wait_confirmation", true)
+                .leaf("eat", Botcraft::EatBlackboard)
+            .end()
+        .end();
+    // clang-format on
+}
+
 std::shared_ptr<Botcraft::BehaviourTree<AdvancedClient>> SurvivalTasks::CreateTree()
 {
     // clang-format off
@@ -41,6 +146,8 @@ std::shared_ptr<Botcraft::BehaviourTree<AdvancedClient>> SurvivalTasks::CreateTr
                     .leaf(SurvivalTasks::InitializeBlocks, 128)
                 .end()
             .end()
+            .tree(CreateSleepTree())
+            .tree(CreateEatTree())
         .end();
     // clang-format on
 }
