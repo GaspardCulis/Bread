d2012a070ae8060fb83833c3cefd6c9ab33c1531
Gaspard Culis <79080828+GaspardCulis@users.noreply.github.com>
2023-05-12 11:47:18 +0000

    SurvivalTasks::InitializeBlocks now seraching bed

--- Bread/src/SurvivalTasks.cpp
+++ Bread/src/SurvivalTasks.cpp
@@ -1,1 +1,24 @@
 #include "SurvivalTasks.hpp"
+
+Botcraft::Status SurvivalTasks::InitializeBlocks(AdvancedClient &client, const int search_radius)
+{
+    Botcraft::Blackboard &b = client.GetBlackboard();
+
+    auto _ = client.findBlocks(
+        [&b](const Block *block, const Position position, std::shared_ptr<World> world) -> bool
+        {
+            Position down = position + Position(0, -1, 0);
+            const std::string &block_name = block->GetBlockstate()->GetName();
+            const std::string bed_suffix = "_bed";
+
+            if (
+                block_name.size() >= bed_suffix.size() &&
+                0 == block_name.compare(block_name.size() - bed_suffix.size(), bed_suffix.size(), bed_suffix) &&
+                world->GetBlock(down)->GetBlockstate()->GetName() == "minecraft:gold_block")
+            {
+                b.Set("SurvivalTasks.bed_pos", position);
+                LOG_INFO(block_name << "found at: " << position << "!");
+            }
+        },
+        search_radius);
+}
